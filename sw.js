281abea9dfcd3c1d51c3c493c69b34015ccda977
Johann Huang <johann.huang.de@gmail.com>
2021-10-28 00:05:19 +0200

    made only cache huge dict app files

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const cacheName = 'QiaSoft-Dictionaries-v1'
+const cacheName = 'QiaSoft-Dictionaries-v1.1'
+const fileNamesToCache = ['jh-dict-cedict.js', 'jh-dict-cfdict.js', 'jh-dict-handedict.js']
 
 self.addEventListener('activate', (event) => {
 	event.waitUntil(self.caches.keys().then((keyList) => {
@@ -10,6 +11,15 @@ self.addEventListener('activate', (event) => {
 })
 
 self.addEventListener('fetch', (event) => {
+	const request = event.request
+
+	const method = request.method
+	if (!['GET'].includes(method)) return
+
+	const url = new URL(request.url)
+	const pathname = url.pathname
+	if (!fileNamesToCache.some(fileName => pathname.endsWith(fileName))) return
+
 	event.respondWith((async () => {
 		let request = await self.caches.match(event.request)
 		if (request) return request
